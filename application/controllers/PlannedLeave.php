7fac1860687eacd60d0f9c95d2afa36c0d74b24d
plaboncosta <plabonjoseph@gmail.com>
2020-07-06 23:45:01 +0600

    planned leave add and update done

---
+++ application/controllers/PlannedLeave.php
@@ -0,0 +1,100 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class PlannedLeave extends CI_Controller
+{
+
+    /**
+     * Index Page for this controller.
+     * Maps to the following URL
+     *         http://example.com/index.php/welcome
+     *    - or -
+     *         http://example.com/index.php/welcome/index
+     *    - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('login_model');
+        $this->load->model('dashboard_model');
+        $this->load->model('employee_model');
+        $this->load->model('loan_model');
+        $this->load->model('settings_model');
+        $this->load->model('leave_model');
+        $this->load->model('PlannedLeave_model');
+    }
+
+    public function index()
+    {
+        if ($this->session->userdata('user_login_access') != false) {
+            $data['user_type'] = $user_type = $this->session->userdata('user_type');
+            $data['leave_types'] = $this->leave_model->GetleavetypeInfo();
+
+            if ($user_type == 'EMPLOYEE') {
+                $data['planned_leave_data'] = $this->PlannedLeave_model->GetPlannedLeaveByUser($this->session->userdata('user_login_id'));
+            } else {
+                $data['planned_leave_data'] = $this->PlannedLeave_model->GetPlannedLeave();
+            }
+
+            $this->load->view('backend/planned-leave', $data);
+        } else {
+            redirect(base_url(), 'refresh');
+        }
+    }
+
+
+    public function addPlannedLeave()
+    {
+        if ($this->session->userdata('user_login_access') != false) {
+            $id = $this->input->post('id');
+            $leave_type_id = $this->input->post('leave_type_id');
+            $leave_from = $this->input->post('leave_from');
+            $leave_to = $this->input->post('leave_to');
+            $remarks = $this->input->post('remarks');
+            $now = new DateTime();
+            $planned_leave = $this->PlannedLeave_model->plannedLeaveById($id);
+
+            $data = array(
+                'leave_type_id' => $leave_type_id,
+                'leave_from' => $leave_from,
+                'leave_to' => $leave_to,
+                'remarks' => $remarks,
+                'em_id' => $this->session->userdata('user_login_id'),
+                'added_by' => $this->session->userdata('name'),
+                'created_at' => $id == '' ? $now->format('Y-m-d H:i:s') : $planned_leave->created_at,
+                'updated_at' => $id != '' ? $now->format('Y-m-d H:i:s') : ''
+            );
+
+            if ($id) {
+                $this->PlannedLeave_model->updatePlannedLeave($id, $data);
+                $this->session->set_flashdata('feedback', 'Successfully Updated');
+            } else {
+                $this->PlannedLeave_model->addPlannedLeave($data);
+                $this->session->set_flashdata('feedback', 'Successfully Added');
+            }
+
+            redirect("PlannedLeave/index");
+        } else {
+            redirect(base_url(), 'refresh');
+        }
+    }
+
+    public function plannedLeaveById()
+    {
+        if ($this->session->userdata('user_login_access') != false) {
+            $id = $this->input->get('id');
+            $data['planned_leave'] = $this->PlannedLeave_model->plannedLeaveById($id);
+            echo json_encode($data);
+        } else {
+            redirect(base_url(), 'refresh');
+        }
+    }
+
+}
---
+++ application/views/backend/planned-leave.php
@@ -0,0 +1,167 @@
+<?php $this->load->view('backend/header'); ?>
+<?php $this->load->view('backend/sidebar'); ?>
+<main>
+    <div class="container-fluid">
+        <div class="row ">
+            <div class="col-12  align-self-center">
+                <div class="sub-header mt-3 py-3 px-3 align-self-center d-sm-flex w-100 rounded">
+                    <div class="w-sm-100 mr-auto"><h4 class="mb-0">Planned Leave</h4></div>
+
+                    <ol class="breadcrumb bg-transparent align-self-center m-0 p-0">
+                        <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Home</a></li>
+                        <li class="breadcrumb-item active"><a href="#">Planned Leave</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 mt-3">
+                <?php if ($this->session->flashdata('feedback')) { ?>
+                    <div class="alert alert-success alert-dismissible show" role="alert">
+                        <strong><?php echo $this->session->flashdata('feedback'); ?></strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                <?php } ?>
+                <?php if ($this->session->flashdata('error')) { ?>
+                    <div class="alert alert-danger alert-dismissible show" role="alert">
+                        <strong><?php echo $this->session->flashdata('error'); ?></strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                <?php } ?>
+
+                <!-- START: Card Data-->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between">
+                                <h4 class="card-title">Planned Leave List</h4>
+
+                                <button class="btn btn-primary rounded-btn mb-2" data-toggle="modal"
+                                        data-target="#planned-leave-modal" onclick="emptyInputValue()">
+                                    <i class="fa fa-plus"></i>
+                                    <span class="d-inline-block pl-1">Add Planned Leave</span>
+                                </button>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="data-table" data-page-length='10'
+                                           class="display table dataTable table-striped table-bordered text-center">
+                                        <thead>
+                                        <tr>
+                                            <th>Leave Type</th>
+                                            <th>Leave From</th>
+                                            <th>Leave To</th>
+                                            <th>Remarks</th>
+                                            <th>Added By</th>
+                                            <th>Created At</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="text-center">
+                                        <?php foreach ($planned_leave_data as $value): ?>
+                                            <tr>
+                                                <td><?php echo $value->leave_type_name ?></td>
+                                                <td><?php echo $value->leave_from ?></td>
+                                                <td><?php echo $value->leave_to ?></td>
+                                                <td><?php echo substr($value->remarks, 0, 50);
+                                                    echo strlen($value->remarks) > 50 ? '...' : ''; ?></td>
+                                                <td><?php echo $value->added_by ?></td>
+                                                <td><?php echo date('jS \of F Y', strtotime($value->created_at)) ?></td>
+                                                <td>
+                                                    <a href="javascript:void(0);" class="btn btn-info rounded-btn"
+                                                       onclick="getPlannedLeaveInfoById(<?php echo $value->id; ?>)">
+                                                        Edit
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END: Card DATA-->
+            </div>
+        </div>
+    </div>
+
+    <div class="modal" id="planned-leave-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel10"
+         aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="myLargeModalLabel10">Planned Leave</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form role="form" method="post" action="addPlannedLeave" id="btnSubmit">
+                    <div class="modal-body">
+                        <div class="form-group row">
+                            <label for="leave_type_id" class="control-label col-md-3">Leave Types</label>
+                            <select name="leave_type_id" id="leave_type_id" class="form-control col-md-8" required>
+                                <option value="">Select Leave Type</option>
+                                <?php
+                                foreach ($leave_types as $record) {
+                                    ?>
+                                    <option value="<?php echo $record->type_id; ?>"><?php echo $record->name; ?></option>
+                                    <?php
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="leave_from" class="control-label col-md-3">Leave From</label>
+                            <input type="date" name="leave_from" id="leave_from" class="form-control col-md-8" required>
+                        </div>
+                        <div class="form-group row">
+                            <label for="leave_to" class="control-label col-md-3">Leave To</label>
+                            <input type="date" name="leave_to" id="leave_to" class="form-control col-md-8" required>
+                        </div>
+                        <div class="form-group row">
+                            <label for="remarks" class="control-label col-md-3">Remarks</label>
+                            <textarea class="form-control col-md-8" name="remarks" id="remarks" rows="4"
+                                      required></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="hidden" name="id" value="">
+                        <button type="button" class="btn btn-danger rounded-btn" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary rounded-btn">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</main>
+<?php $this->load->view('backend/footer'); ?>
+<script>
+    function getPlannedLeaveInfoById(id) {
+        $('#btnSubmit').trigger("reset");
+        $('#planned-leave-modal').modal('show');
+
+        $.ajax({
+            url: 'plannedLeaveById?id=' + id,
+            method: 'GET',
+            data: '',
+            dataType: 'json',
+        }).done(function (response) {
+            $('#btnSubmit').find('[name="id"]').val(response.planned_leave.id).end();
+            $('#btnSubmit').find('[name="leave_type_id"]').val(response.planned_leave.leave_type_id).end();
+            $('#btnSubmit').find('[name="leave_from"]').val(response.planned_leave.leave_from).end();
+            $('#btnSubmit').find('[name="leave_to"]').val(response.planned_leave.leave_to).end();
+            $('#btnSubmit').find('[name="remarks"]').val(response.planned_leave.remarks).end();
+        });
+    }
+
+    function emptyInputValue() {
+        $('#btnSubmit').trigger("reset");
+    }
+</script>
