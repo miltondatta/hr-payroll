51e618fe82b5372813b158edf3c8046bc0d6ce06
Shahadat Hossain <shahadat@clouditbd.com>
2020-07-10 23:52:02 +0600

    update role m

--- application/controllers/UserManagement.php
+++ application/controllers/UserManagement.php
@@ -22,8 +22,19 @@ class UserManagement extends CI_Controller{
 
     public function AssignMenuEdit(){
         if($this->session->userdata('user_login_access') != false){
-            $role_id        = base64_decode($this->input->get('id'));
-            $this->load->view('backend/role_management_edit');
+            $role_id     = $data['role_id']   = base64_decode($this->input->get('id'));
+            $assign_menu = $this->settings_model->checkMenuAssign($role_id);
+            if($assign_menu) {
+               $data['menu_assign_id'] = $assign_menu->id; 
+               $data['menu_id']        = explode(',', $assign_menu->menu_id); 
+            } else {
+                $data['menu_assign_id'] = 0;
+                $data['menu_id']        = [];
+            }
+            $menus    = [];
+            $data['mainMenu'] = $this->settings_model->getMenu(0);
+            //$subMenu  = $this->settings_model->getMenu($record->id); 
+            $this->load->view('backend/role_management_edit', $data);
         } else {
             redirect(base_url(), 'refresh');
         }    
