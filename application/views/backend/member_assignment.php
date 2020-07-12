9eb10a31e1eed2fe547f529bd797b259e1b33a2d
shuvo.babl <shuvo.babl@gmail.com>
2020-07-12 12:52:05 +0600

    heading change

--- application/views/backend/member_assignment.php
+++ application/views/backend/member_assignment.php
@@ -5,7 +5,7 @@
             <div class="row ">
                 <div class="col-12  align-self-center">
                     <div class="sub-header mt-3 py-3 px-3 align-self-center d-sm-flex w-100 rounded">
-                        <div class="w-sm-100 mr-auto"><h4 class="mb-0">Training Agency</h4></div>
+                        <div class="w-sm-100 mr-auto"><h4 class="mb-0">Member Assignment</h4></div>
 
                         <ol class="breadcrumb bg-transparent align-self-center m-0 p-0">
                             <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Home</a></li>
